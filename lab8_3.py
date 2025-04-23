c2ae44fa0b314996786ada2572fc2c6715656351
shakyracc <shakyracornelius@gmail.com>
2025-04-22 21:29:59 -0400

    add question 3 solutions

--- lab8_3.py
+++ lab8_3.py
@@ -1,21 +1,44 @@
-import time, random
-from sorting_algorithms import selection_sort, bubble_sort, merge_sort, quick_sort, heap_sort, radix_sort
+import sorting
+import time
+import random
 
-def generate_data(n):
-    return [random.randint(0, 1000000) for _ in range(n)]
-
+# List of array sizes to test
 sizes = [50000, 100000, 150000, 200000, 250000, 300000]
-sorts = [("Selection", selection_sort), ("Bubble", bubble_sort), ("Merge", merge_sort), 
-         ("Quick", quick_sort), ("Heap", heap_sort), ("Radix", radix_sort)]
 
-print("Array Size\t" + "\t".join([s[0] for s in sorts]))
+# put sorting algos in a dictionary
+algorithms = {
+    "Selection": sorting.selection,
+    "Bubble": sorting.bubble,
+    "Merge": sorting.merge,
+    "Quick": sorting.quick,
+    "Heap": sorting.heap,
+    "Radix": sorting.radix
+}
+
+# Print table header
+print("Array Size | Selection | Bubble | Merge | Quick | Heap | Radix")
 
 for size in sizes:
-    print(f"{size}\t", end="")
-    for name, func in sorts:
-        data = generate_data(size)
+    # Make a list of random numbers. list is length of size
+    numbers = [random.randint(1, 1000000) for i in range(size)]
+    
+    # put current list size in first column
+    print(f"{size:<10}", end=" ")
+
+    # Try each sorting algorithm
+    for name in algorithms:
+        sort_function = algorithms[name]
+
+        # Make a fresh copy of the numbers list to ensure fair calculation
+        copy = numbers.copy()
+
+        # calculate excecution time
         start = time.time()
-        func(data.copy())  # make sure original isn't changed
+        sort_function(copy)
         end = time.time()
-        print(f"{end - start:.2f}s", end="\t")
-    print()
+
+        # Print rxecution time
+        time_taken = end - start
+        print(f"{time_taken:.2f}      ", end=" ")
+
+    print()  # new line
